40cabbb085d926c45199a395f55b1fec2267798d
Student <160208713+Alisha000111@users.noreply.github.com>
2024-03-28 11:02:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// 79.............
+var car = {
+    make: "Suzuki",
+    model: "Wagon R",
+    year: 2023
+};
+console.log(car.model);
+// 80..............
+var car = {
+    make: "Suzuki",
+    model: "Wagon R",
+    year: 2023
+};
+car.color = "red";
+car.year = 2023;
+console.log(car);
+// 81..............
+var car = {
+    make: "Suzuki",
+    model: "Wagon R",
+    year: 2023
+};
+car.color = "red";
+car.year = 2023;
+console.log(car);
---
+++ app.ts
@@ -0,0 +1,25 @@
+// 79.............
+ let car = {
+    make: "Suzuki",
+    model: "Wagon R",
+    year: 2023
+ };
+ console.log(car.model);
+// 80..............
+ let car = {
+     make: "Suzuki",
+     model: "Wagon R",
+    year: 2023
+ };
+ car.color = "red"; 
+ car.year = 2023; 
+ console.log(car); 
+// 81..............
+let car = {
+    make: "Suzuki",
+    model: "Wagon R",
+    year: 2023
+};
+car.color = "red"; 
+car.year = 2023;
+console.log(car); 
